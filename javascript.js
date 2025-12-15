f3476ae338a177e4a10ba8a583515b3a9c8926d5
heathdav111 <lewisderek225@gmail.com>
2025-12-14 21:16:29 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
+const hotspots = document.querySelectorAll(".hotspot");
+const infoPanels = document.querySelectorAll(".hotspot-info");
+
+hotspots.forEach(hotspot => {
+    hotspot.addEventListener("click", () => {
+
+    
+        infoPanels.forEach(panel => {
+            panel.classList.remove("active");
+        });
+
+        const targetId = hotspot.getAttribute("data-target");
+        const targetPanel = document.getElementById(targetId);
+
+        if (targetPanel) {
+            targetPanel.classList.add("active");
+            targetPanel.scrollIntoView({ behavior: "smooth", block: "center" });
+        }
+    });
+});
 
